226eb6c719cfd6d097e593b6fa06f6d107d51463
bladeclara42 <71927457+bladeclara42@users.noreply.github.com>
2021-06-20 12:38:47 +0900

    first commit

---
+++ database/migrations/2021_06_20_025717_create_shopping_cart_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+            $table->unsignedBigInteger('id_product');
+            $table->foreign('id_product')->references('id')->on('product');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_cart');
+    }
+}
